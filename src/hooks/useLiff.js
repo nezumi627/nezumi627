d3e41ac5616e02e62677bf04611d1b5a63e51fcd
nezumi627 <114066722+nezumi627@users.noreply.github.com>
2022-11-28 20:56:03 +0900

    Create useLiff.js

---
+++ src/hooks/useLiff.js
@@ -0,0 +1,7 @@
+import { useState, useEffect } from 'react'; import { liff } from '../lib/liff'; function useLiff({ liffId }) { const [loading,
+setLoading] = useState(false); const [error, setError] = useState(null); const [profile, setProfile] = useState(null); const
+initLiff = async ({ liffId }) => { // 省略 }; const fetchProfile = async () => { // 省略 }; // 送信する内容を引数で受け取る const sendMessage
+= async ({ text }) => { setLoading(true); try { // LIFF APIのsendMessagesを実行 liff.sendMessages([{ type: 'text', text }]);
+console.log(`success send message: ${text}`); } catch (error) { console.log({ error }); setError(error); } finally { setLoading(false);
+} }; useEffect(() => { initLiff({ liffId }); }, [liffId]); // 修正 return { loading, error, fetchProfile, profile, sendMessage
+}; } export default useLiff;
