4aa677543e1f992fe2f91699a1ab6f57453e455c
nezumi627 <114066722+nezumi627@users.noreply.github.com>
2022-11-28 21:40:15 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
 // useStateを追加 import React, { useState } from 'react'; import useLiff from './hooks/useLiff'; // 自身のLIFF IDを設定 const liffId
-= '1234567890-abcedfgh'; function App() { // 修正 const { loading, error, profile, fetchProfile, sendMessage } = useLiff({
-liffId }); // メッセージのstateを追加 const [text, setText] = useState(''); if (loading) return
-<p>...loading</p>; if (error) return
-<p>{error.message}</p>; return (
-<div>
-     <h1>Hello LIFF</h1>
-     <section> <button onClick={()=> fetchProfile()}>Get Profile</button> {profile && (
-          <div>
-               <p>UserID: {profile.userId}</p>
-               <p>DisplayName: {profile.displayName}</p>
-               <p> Picture: <br /> <img src={profile.pictureUrl} height="300" width="300" /> </p> {profile.statusMessage && (
-               <p>StatusMessage: {profile.statusMessage}</p> )} </div> )} </section> {/* メッセージの入力域と送信ボタンを追加 */}
-     <section> <input onChange={e=> setText(e.target.value)} /> <button onClick={()=> sendMessage({ text })}>Send</button> </section>
-</div> ); } export default App;
+= '1234567890-abcedfgh';
+
+function App() { // 修正 const { loading, error, profile, fetchProfile, sendMessage } = useLiff({
+  liffId
+}); // メッセージのstateを追加 const [text, setText] = useState(''); if (loading) return
+< p > ...loading < /p>; if (error) return < p > {
+  error.message
+} < /p>; return ( < div > < h1 > Hello LIFF < /h1> < section > < button onClick = {
+  () => fetchProfile()
+} > Get Profile < /button> {profile && ( < div > < p > UserID: {
+  profile.userId
+} < /p> < p > DisplayName: {
+  profile.displayName
+} < /p> < p > Picture: < br / > < img src = {
+  profile.pictureUrl
+}
+height = "300"
+width = "300" / > < /p> {profile.statusMessage && ( < p > StatusMessage: {
+profile.statusMessage
+} < /p> )} </div > )
+} < /section> {/ * メッセージの入力域と送信ボタンを追加 * /} < section > < input onChange = {
+  e => setText(e.target.value)
+}
+/> <button onClick={()=> sendMessage({ text })}>Send</button > < /section> < /div> ); } export default App;
